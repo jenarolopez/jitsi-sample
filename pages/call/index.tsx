7f6eeb7e366f6e2929dc8866704cf5c97e62d6f6
Jenaro <jenjenlopez37@gmail.com>
2021-10-13 20:06:59 +0800

    jitsi config update

--- pages/call/index.tsx
+++ pages/call/index.tsx
@@ -1,54 +1,16 @@
-import React, { useState, Component, useEffect } from 'react'
+import React, { useState, Component, useEffect, useCallback } from 'react'
 import Head from 'next/head'
+
+
+
 
 const JitsiComponent = () => {
 
 
-  const startMeet = () => {
-    const options = {
-      roomName: 'bwb-bfqi-vmg',
-      width: '100%',
-      height: 500,
-      configOverwrite: { prejoinPageEnabled: false },
-      interfaceConfigOverwrite: {
-        // overwrite interface properties
-      },
-      parentNode: document.querySelector('#jitsi-iframe'),
-      userInfo: {
-        displayName: 'Akash Verma'
-      }
-    }
-    new window.JitsiMeetExternalAPI('meet.jit.si', options);
-
-
-  }
-
-
-
-  useEffect(() => {
-    if (window.JitsiMeetExternalAPI) {
-      startMeet();
-    } else {
-      alert('JitsiMeetExternalAPI not loaded');
-    }
-  }, [])
-
-
-  return <>
-    <header className="nav-bar">
-      <p className="item-left heading">Jitsi React</p>
-    </header>
-    <script src='https://meet.jit.si/external_api.js'></script>
-    <div id="jitsi-iframe"></div>
-    <div className="item-center">
-      <span>Custom Controls</span>
-    </div>
-    <div className="item-center">
-    </div>
-
-  </>
-
+  return <div>Some shit</div>
 
 }
+
+
 
 export default JitsiComponent;
---
+++ pages/calling/index.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import Jitsi from 'react-jitsi'
+
+
+const App = () => {
+
+    const [displayName, setDisplayName] = useState('')
+    const [roomName, setRoomName] = useState('')
+    const [password, setPassword] = useState('')
+    const [onCall, setOnCall] = useState(false)
+    const config = {
+        defaultLanguage: "es",
+        prejoinPageEnabled: false
+    };
+    return onCall
+        ? (
+            <Jitsi
+                containerStyle={{ width: '1200px', height: '800px' }}
+                roomName={roomName}
+                displayName={displayName}
+                password={password}
+                config={config}
+                onAPILoad={JitsiMeetAPI => console.log('Good Morning everyone!')}
+            />)
+        : (
+            <>
+                <h1>Crate a Meeting</h1>
+                <input type='text' placeholder='Room name' value={roomName} onChange={e => setRoomName(e.target.value)} />
+                <input type='text' placeholder='Your name' value={displayName} onChange={e => setDisplayName(e.target.value)} />
+                <button onClick={() => setOnCall(true)}> Let&apos;s start!</button>
+            </>
+        )
+
+}
+
+export default App
