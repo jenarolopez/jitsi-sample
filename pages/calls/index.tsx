b79ef139a07bc0f0d6f1ac71d83c725de8929e9c
Jenaro <jenjenlopez37@gmail.com>
2021-10-13 20:08:53 +0800

    updates

--- pages/calls/index.tsx
+++ pages/calls/index.tsx
@@ -1,7 +1,9 @@
 
 const Calls = () => {
     return <div>
-        <iframe style={{ height: "100vh", width: "100vw", border: "0px" }} allow="camera; microphone; fullscreen; display-capture" src="https://8x8.vc/mycompany/mymeeting"></iframe>
+        <iframe style={{ height: "100vh", width: "100vw", border: "0px" }}
+            allow="camera; microphone; fullscreen; display-capture"
+            src="https://8x8.vc/mycompany/mymeeting"></iframe>
     </div>
 }
 export default Calls
